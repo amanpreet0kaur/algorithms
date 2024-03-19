082bd1f6a7ac4fb805da20d5a58bc1c1c7b674b0
amanpreet0kaur <123165272+amanpreet0kaur@users.noreply.github.com>
2024-03-19 15:30:38 +0530

    Update selectionsort.py

--- selectionsort.py
+++ selectionsort.py
@@ -1,1 +1,50 @@
+import random
+import time
+import matplotlib.pyplot as plt
+listx=[]
+listy=[]
+def selection_sort(arr, n):
+  for ind in range(n):
+    min=ind
+    for j in range(ind+1,n):
+       if arr[j] < arr[min]:
+                min = j
+         # swapping the elements to sort the array
+                (arr[ind], arr[min]) = (arr[min], arr[ind])
+  return arr
+
+list1=[]
+list2=[]
+
+def random_generator(n):
+  arr=[0]*n
+  a=1
+  for i in range(n):
+    arr[i]=(random.randint(a, n))
+  return arr
+
+n=100
+while(n<=10000):
+  arr=random_generator(n)
+  t_start= time.time()
+  for i in range (10):
+    selection_sort(arr,n)
+  t_end=time.time()
+  avg_time=(t_end-t_start)/10
+  print("time: ",avg_time," for ",n," inputs")
+  listx.append(avg_time*1000)
+  listy.append(n)
+  list1.append(n)
+  list2.append(n*n)
+  n=n*2
+
+plt.scatter(listy,listx)
+plt.xlabel("Number of inputs")
+plt.ylabel("time")
+plt.show()
+plt.scatter(list1,list2)
+plt.xlabel("n")
+plt.ylabel(" n sqaure")
+plt.show()
+
 
